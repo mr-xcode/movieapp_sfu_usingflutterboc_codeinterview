d1a8af0ce8a53a569daf6b84bb9e3fb3344b08e4
mrxcode <phyowaiyan.dev@gmail.com>
2024-05-07 16:50:34 +0630

    adding blocbuild to movie_details_page

--- lib/bloc/moviedetails_bloc.dart
+++ lib/bloc/moviedetails_bloc.dart
@@ -16,8 +16,31 @@ class MoviedetailsBloc extends Bloc<MoviedetailsEvent, MoviedetailsState> {
     on<MoviedetailsEvent>((event, emit) {
       // TODO: implement event handler
     });
+
     on<MyMoviesDeailsEvent>((event, emit) async {
-      emit(MovieDetailLoading());
+      Map<String, Movie> moviesList = {};
+      var url;
+      var response;
+
+      try {
+        url = Uri.parse(
+            'https://api.themoviedb.org/3/movie/popular?api_key=19d6149f34738ec93c495cd0527246ae&language=en-US&page=1');
+
+        var response = await http.get(url);
+
+        if (response.statusCode == 200) {
+          var jsonData = jsonDecode(response.body);
+          List<dynamic> results = jsonData['results'];
+
+          List<Movie> moviesList =
+              results.map((movieData) => Movie.fromJson(movieData)).toList();
+          emit(MovieDetailLoaded(moviesList[0]));
+        } else {
+          emit(MovieDetailError());
+        }
+      } catch (e) {
+        emit(MovieDetailError());
+      }
     });
   }
 }
--- lib/ui/pages/movie_details_page.dart
+++ lib/ui/pages/movie_details_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:getwidget/components/list_tile/gf_list_tile.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:movieapp_sfu_usingflutterboc_codeinterview/bloc/moviedetails_bloc.dart';
 import 'package:movieapp_sfu_usingflutterboc_codeinterview/ui/widgets/helper_widgets.dart';
 
 class MovieDetailsPage extends StatelessWidget {
@@ -7,86 +11,111 @@ class MovieDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.white),
-        title: const Text(
-          'Spider-Man No Way..',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        backgroundColor: Theme.of(context).primaryColor,
-        centerTitle: true,
-        actions: const [
-          Icon(
-            Icons.favorite,
-            color: Colors.red,
-          ),
-          SizedBox(
-            width: 10,
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          color: Theme.of(context).primaryColor,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 50,
+      body: BlocBuilder<MoviedetailsBloc, MoviedetailsState>(
+          builder: (context, state) {
+        if (state is MoviedetailsInitial) {
+          return GFLoader();
+        }
+        if (state is MovieDetailLoading) {
+          return GFLoader();
+        }
+        if (state is MovieDetailLoaded) {
+          return SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              color: Theme.of(context).primaryColor,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 63,
+                    color: Theme.of(context).primaryColor,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "Title Section",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16),
+                        ),
+                        Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+
+                  // Image
+                  Container(
+                    width: 205,
+                    height: 287,
+                    child: Image.asset(
+                      'assets/images/spider_man.png',
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+
+                  // year, time, action
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  yearMinuteAction(),
+
+                  // play, download, share
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  playDownloadShare(),
+
+                  // Action row
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  actionRow(),
+
+                  // Story Line
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  storyLine(),
+
+                  // Cast and Crew
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  castAndCrew(),
+
+                  //TestintCode
+
+                  //EndHere
+                ],
               ),
-
-              // Image
-              Container(
-                width: 205,
-                height: 287,
-                child: Image.asset(
-                  'assets/images/spider_man.png',
-                  fit: BoxFit.fill,
-                ),
-              ),
-
-              // year, time, action
-              const SizedBox(
-                height: 10,
-              ),
-              yearMinuteAction(),
-
-              // play, download, share
-              const SizedBox(
-                height: 10,
-              ),
-              playDownloadShare(),
-
-              // Action row
-              const SizedBox(
-                height: 20,
-              ),
-              actionRow(),
-
-              // Story Line
-              const SizedBox(
-                height: 20,
-              ),
-              storyLine(),
-
-              // Cast and Crew
-              const SizedBox(
-                height: 10,
-              ),
-              castAndCrew(),
-
-              //TestintCode
-
-              //EndHere
-            ],
-          ),
-        ),
-      ),
+            ),
+          );
+        }
+        if (state is MovieDetailError) {
+          return Center(
+            child: Text('Error'),
+          );
+        }
+        return Container();
+      }),
     );
   }
 }
